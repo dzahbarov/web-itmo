e1edde88237b4ceea96503678f8c0c2fc3039550
Dzhakhbarov_Vladimir <dzahbarovvladimir@gmail.com>
2021-10-09 16:44:17 +0300

    change msg and style

--- hw3/wp3/src/main/java/ru/itmo/wp/servlet/MessageServlet.java
+++ hw3/wp3/src/main/java/ru/itmo/wp/servlet/MessageServlet.java
@@ -8,7 +8,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 /**
@@ -18,7 +17,6 @@ import java.util.ArrayList;
 public class MessageServlet extends HttpServlet {
 
     private final ArrayList<Message> messages = new ArrayList<>();
-    private final static String UTF_8 = StandardCharsets.UTF_8.name();
 
     static class Message {
         final String user;
@@ -32,63 +30,31 @@ public class MessageServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setCharacterEncoding(UTF_8);
-        resp.setCharacterEncoding(UTF_8);
-
         if (req.getRequestURI().equals("/message/auth")) {
-
             HttpSession session = req.getSession();
             String user = req.getParameter("user");
 
-            if (user == null) user = (String) session.getAttribute("user");
             if (user == null) {
-                resp.sendError(HttpServletResponse.SC_UNAUTHORIZED);
-                return;
+                user = (String) session.getAttribute("user");
             }
+            if (user == null) user = "";
 
             session.setAttribute("user", user);
             resp.setContentType("application/json");
-
             resp.getWriter().print(new Gson().toJson(user));
             resp.getWriter().flush();
 
         } else if (req.getRequestURI().equals("/message/findAll")) {
-
-            if (req.getSession().getAttribute("user") == null) {
-                resp.sendError(HttpServletResponse.SC_UNAUTHORIZED);
-                return;
-            }
-
             resp.setContentType("application/json");
-            resp.getWriter().print(new Gson().toJson(messages));
+            resp.getWriter().print( new Gson().toJson(messages));
             resp.getWriter().flush();
 
         } else if (req.getRequestURI().equals("/message/add")) {
             HttpSession session = req.getSession();
-
-            if (session.getAttribute("user") == null) {
-                resp.sendError(HttpServletResponse.SC_UNAUTHORIZED);
-                return;
-            }
-
-            if (req.getParameter("text") == null || isBlank(req.getParameter("text"))) {
-                resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
-                return;
-            }
-
-            Message message = new Message((String) session.getAttribute("user"),
-                    req.getParameter("text"));
+            String user = (String) session.getAttribute("user");
+            Message message = new Message(user, req.getParameter("text"));
             messages.add(message);
         }
     }
 
-    // since 11
-    private boolean isBlank(String str) {
-        for (char c : str.toCharArray()) {
-            if (!Character.isWhitespace(c)) {
-                return false;
-            }
-        }
-        return true;
-    }
 }
