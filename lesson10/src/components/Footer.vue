08255d38134ea6a57dd25868a1628929d5bd8373
Dzhakhbarov_Vladimir <dzahbarovvladimir@gmail.com>
2021-12-06 18:46:06 +0300

    fix hw9 and add hw10

---
+++ lesson10/src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer>
+    <div class="userStat">
+      Number of users: {{ usersNumber }}
+    </div>
+    <div class="postStat">
+      Number of posts: {{ postsNumber }}
+    </div>
+    <a href="/">Codeforces</a> &copy; 2021 by Mike Mirzayanov
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  props: ["postsNumber", "usersNumber"]
+}
+</script>
+
+<style scoped>
+
+</style>
